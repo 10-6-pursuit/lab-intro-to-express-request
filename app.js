016dddd5303296a4b2fd1e79f89d27f972a745d1
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-19 18:14:39 -0400

    complete 99 Little Bugs In the Code section

--- app.js
+++ app.js
@@ -4,14 +4,29 @@ const app = express();
 
 // ROUTES
 app.get("/", (req, res) => {
-    res.send("Welcome to 99 Little Bugs In the Code");
+    res.send(`Welcome to 99 Little Bugs In the Code & Poke-Express`);
   });
 
-// To get "Congratulations on starting a new project called jumping-joyous-jellybean!"
-app.get("/:verb/:adjective/:noun", (req, res) => {
-    const { verb, adjective, noun } = req.params
-    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
-}), 
+
+app.get("/bugs", (req, res) => {
+    res.send(`99 little bugs in the code <a href="/bugs/101/">pull one down, patch it around</a>`)
+})
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+    const { numberOfBugs } = req.params
+    if ( Number(numberOfBugs) > 200 ) {
+        res.send(`${Number(numberOfBugs)} little bugs in the code <a href="/bugs">too many bugs, have to star over!</a>`)
+    } else {
+        res.send(`${Number(numberOfBugs)} little bugs in the code <a href="/bugs/${Number(numberOfBugs) + 2}/">pull one down, patch it around</a>`)
+    }
+})
+// // To get "Congratulations on starting a new project called jumping-joyous-jellybean!"
+// app.get("/:verb/:adjective/:noun", (req, res) => {
+//     const { verb, adjective, noun } = req.params
+//     res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+// }), 
+
+
 
   // EXPORT
 module.exports = app;
