db6138700c98a24b87b309bb03819715733252a1
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-19 17:57:13 -0400

    complete simple activity with verb, adjectiv, and noun

--- app.js
+++ app.js
@@ -7,5 +7,11 @@ app.get("/", (req, res) => {
     res.send("Welcome to 99 Little Bugs In the Code");
   });
 
+// To get "Congratulations on starting a new project called jumping-joyous-jellybean!"
+app.get("/:verb/:adjective/:noun", (req, res) => {
+    const { verb, adjective, noun } = req.params
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+}), 
+
   // EXPORT
 module.exports = app;
