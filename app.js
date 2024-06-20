d4101d48a300424f0c2a54fad1d9f47626e6eff4
ayoublos <abelibel@pursuit.org>
2024-06-20 10:05:07 -0700

    finished verb-adjective-noun-route

---
+++ app.js
@@ -0,0 +1,25 @@
+const express=require(`express`);
+const app=express()
+
+app.get(`/bugs` ,(req,res)=>{
+ 
+    res.send("99 little bugs in the code"
+        
+    )
+})
+app.get(`/bugs/:numberofbugs` ,(req,res)=>{
+    let number=req.params.numberofbugs
+ 
+    res.send(`${number} little bugs in the code`)
+})
+app.get(`/:verb/:adjective/:noun` ,(req,res)=>{
+    let verb=req.params.verb
+    let adjective=req.params.adjective
+    let noun=req.params.noun
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun||``}!`)
+})
+
+
+
+module.exports=app
+
