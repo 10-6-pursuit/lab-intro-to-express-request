0021dab270a4eae0ef13c6bbe2b2c0f270bcb617
AntCannon <anthonycannonier@gmail.com>
2024-06-23 13:13:28 -0400

    Add number of bugs route
    1. get url params
    2. create link to next page
    3. create link to home page
    4. add logic

--- app.js
+++ app.js
@@ -6,15 +6,27 @@ app.get("/", (req, res) => {
 })
 
 // new project name generator
-app.get('/:verb/:adjective/:noun', (req, res) => {
-  const { verb, adjective, noun} = req.params
-  res.send(`Congratulations on starting a new project ${verb}-${adjective}-${noun}`)
-})
+// app.get('/:verb/:adjective/:noun', (req, res) => {
+//   const { verb, adjective, noun} = req.params
+//   res.send(`Congratulations on starting a new project ${verb}-${adjective}-${noun}`)
+// })
 
 // 99 little bugs
 
 app.get('/bugs', (req, res) => {
   res.send(`99 little bugs in the code<br>
-    <a href="/bugs/101">pull one down, patch it around</a>`)
+    99 little bugs<br>
+    <a href="/bugs/${+101}">pull one down, patch it around</a>`)
+})
+
+app.get('/bugs/:numBugs', (req, res) => {
+  const numBugs = +req.params.numBugs
+  const pull = `${numBugs} little bugs in the code<br>
+    ${numBugs} little bugs<br>
+    <a href="/bugs/${numBugs + 2}">pull one down, patch it around</a>`
+  const tooMany = `${numBugs} little bugs in the code<br>
+    ${numBugs} little bugs<br>
+    <a href="/bugs">To many little bugs!! Start over.</a>`
+  res.send(`${numBugs > 200 ? tooMany : pull}`)
 })
 module.exports = app
