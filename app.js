59e0a37719213250c7c7f1b2aa312c60647d17dd
AntCannon <anthonycannonier@gmail.com>
2024-06-23 12:53:35 -0400

    Add bugs route
    1. add route
    2. add link to bugs/101

--- app.js
+++ app.js
@@ -10,4 +10,11 @@ app.get('/:verb/:adjective/:noun', (req, res) => {
   const { verb, adjective, noun} = req.params
   res.send(`Congratulations on starting a new project ${verb}-${adjective}-${noun}`)
 })
+
+// 99 little bugs
+
+app.get('/bugs', (req, res) => {
+  res.send(`99 little bugs in the code<br>
+    <a href="/bugs/101">pull one down, patch it around</a>`)
+})
 module.exports = app
