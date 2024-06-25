6f57a9827da00a078de7aa6b5db988a0bb7b5623
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-06-24 21:55:39 -0400

    done

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ const express = require('express');
 const app = express();
 const pokemon = require('./models/pokemon.json');
 
-// Welcome route
 app.get('/', (req, res) => {
   res.send('Welcome 99 Pokemon');
 });
 
-// Bugs route
 app.get('/bugs', (req, res) => {
   res.send('<h1>99 little bugs in the code</h1><a href="/bugs/101">Pull one down, patch it around</a>');
 });
 
-// Bugs with number route
 app.get('/bugs/:numberOfBugs', (req, res) => {
   const numberOfBugs = parseInt(req.params.numberOfBugs);
   if (numberOfBugs >= 200) {
@@ -22,19 +19,16 @@ app.get('/bugs/:numberOfBugs', (req, res) => {
   }
 });
 
-// List all pokemon
 app.get('/pokemon', (req, res) => {
   res.json(pokemon);
 });
 
-// Search pokemon by name
 app.get('/pokemon/search', (req, res) => {
   const name = req.query.name.toLowerCase();
   const foundPokemon = pokemon.filter(p => p.name.toLowerCase() === name);
   res.json(foundPokemon);
 });
 
-// Get pokemon by index
 app.get('/pokemon/:indexOfArray', (req, res) => {
   const index = parseInt(req.params.indexOfArray);
   if (index >= 0 && index < pokemon.length) {
@@ -44,7 +38,6 @@ app.get('/pokemon/:indexOfArray', (req, res) => {
   }
 });
 
-// New project route
 app.get('/:verb/:adjective/:noun', (req, res) => {
   const { verb, adjective, noun } = req.params;
   res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
