b9e27bef67a1db8ce70353b1b9035cd8b12132b9
gerardjr42 <ggarciajr12@gmail.com>
2024-07-05 04:05:51 -0400

    Create Pokemon Routes 1. Create route for all Pokemon 2. Create route for specific pokemon by index 3. Create route for specific pokemon by name 4. Bonus: Add links to all pokemon to route to a prettier version of poke details 4. Bonus: Add images for each route under /pokemon-pretty/:indexOfArray

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+//Use Pokemon JSON
+const pokemon = require("./models/pokemon.json");
 
 const app = express();
 
+//Home Route
 app.get("/", (req, res) => {
   res.status(200).send("Welcome!");
 });
@@ -36,4 +39,65 @@ app.get("/bugs/:numberOfBugs", (req, res) => {
   }
 });
 
-module.exports = app
+
+//Poke-Express
+
+//All Pokemon
+app.get("/pokemon", (req, res) => {
+  res.status(200).send(pokemon);
+})
+
+// All Pokemon Prettier
+app.get("/pokemon-pretty", (req, res) => {
+  if(pokemon) {
+    let links = "";
+    for(let i = 0; i < pokemon.length; i++) {
+      links += `<a href="/pokemon-pretty/${i}">${pokemon[i].name}: ${i}</a><br>`
+    }
+    res.status(200).send(links);
+  }
+})
+
+//Specific Pokemon Prettier by Index
+app.get("/pokemon-pretty/:indexOfArray", (req, res) => {
+  const { indexOfArray } = req.params;
+  const chosenPokemon = pokemon[indexOfArray]
+
+  if(chosenPokemon) {
+
+    res.status(200).send(`
+      <a>${chosenPokemon.name}</a><br>
+      <img src="${chosenPokemon.img}" alt="${chosenPokemon.name} image"><br>
+      <a>Type:</a><br>
+      <a>${chosenPokemon.type}</a><br>
+    `)
+  } else {
+    res.status(404).send(`sorry, no pokemon found at /pokemon/${indexOfArray}`);
+  }
+})
+
+//Search by Name
+app.get("/pokemon/search", (req, res) => {
+  const { name } = req.query;
+  const pokeName = pokemon.find((poke) => poke.name.toLowerCase() === name.toLowerCase());
+
+  if(pokeName) {
+    res.status(200).send(pokeName);
+  } else {
+    res.status(404).send(`Pokemon with name: "${name}" cannot be found`)
+  }
+});
+
+//Single Pokemon by Index
+app.get("/pokemon/:indexOfArray", (req, res) => {
+  const { indexOfArray } = req.params;
+  const chosenPokemon = pokemon[indexOfArray]
+
+  if(chosenPokemon) {
+    res.status(200).send(chosenPokemon)
+  } else {
+    res.status(404).send(`sorry, no pokemon found at /pokemon/${indexOfArray}`);
+  }
+});
+
+module.exports = app;
